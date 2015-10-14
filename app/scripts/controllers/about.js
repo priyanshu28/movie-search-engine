5621d2ae8539fd3501105692cb90d014074be9be
priyanshu91 <priyanshu.tiwari@canvera.com>
2015-10-14 14:22:32 +0530

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moviesSearchEngineApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the moviesSearchEngineApp
+ */
+angular.module('moviesSearchEngineApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moviesSearchEngineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the moviesSearchEngineApp
+ */
+angular.module('moviesSearchEngineApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
