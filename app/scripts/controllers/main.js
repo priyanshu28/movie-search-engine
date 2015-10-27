4d5b4f0fc8c08346316ccaa4bf8f80f10ffb1087
priyanshu91 <priyanshu.tiwari@canvera.com>
2015-10-27 15:02:16 +0530

    Movies API call implementation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the moviesSearchEngineApp
  */
 angular.module('moviesSearchEngineApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function($scope, $http) {
+
+    $scope.search = "The Avengers";
+
+    // $scope.data = "";
+    $scope.change = function() {
+      $http.get('http://www.omdbapi.com/?t=' + $scope.search).success(function(data) {
+        $scope.data = data;
+      	console.log($scope.data);
+      });
+    };
+
+    $scope.change();
+
+
   });
